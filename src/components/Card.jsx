3748de923a7f9c2532d0fa8acd4effe3d4ff345b
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-01-09 02:11:19 -0300

    feature favoritos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,43 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
 import './Card.css'
 
-const Card = ({data}) => {
+const Card = ({ data }) => {
 
   // const colors = data.types ? [data.types[0], data.types[1] || data.types[0]] : ['unknown', 'unknown']
   const colors = [data.types[0], data.types[1] || data.types[0]]
+  const [favourite, setFavourite] = useState(false)
+
+  useEffect(()=>{
+    setFavourite(false)
+    const favs = localStorage.getItem('favs')
+    if (favs && JSON.parse(favs).includes(data.order)) {
+      setFavourite(true)
+    }
+  },[data])
+
+  const handleFav = () => {
+    const favs = JSON.parse(localStorage.getItem('favs'))
+    let newFavs
+    if (favourite) {
+      newFavs = favs.filter(f => f !== data.order)
+    } else {
+      newFavs = favs ? favs.concat(data.order) : [data.order]
+    }
+    localStorage.setItem('favs', JSON.stringify(newFavs))
+    setFavourite(!favourite)
+  }
 
   return (
     <div className="card">
-      <div className="card-body" style={{backgroundImage: `linear-gradient(45deg, var(--type-${colors[0]}), var(--type-${colors[1]}))`}} >
+      <span className='fav' onClick={handleFav}>{favourite ? '🧡' : '🖤'}</span>
+      <div className="card-body" style={{ backgroundImage: `linear-gradient(45deg, var(--type-${colors[0]}), var(--type-${colors[1]}))` }} >
         {/* <div className="avatar"></div> */}
         <img className="avatar" src={data.image} alt={data.name} />
         <h2 className="card-body-title">
           {data.name} <span>#{data.order}</span>
         </h2>
-        <p className="card-body-subtitle">{ data.types[0] }{ data.types[1] ? ` / ${data.types[1]}` : '' }</p>
+        <p className="card-body-subtitle">{data.types[0]}{data.types[1] ? ` / ${data.types[1]}` : ''}</p>
       </div>
       <div className="card-footer">
         <div className="card-footer-item">
